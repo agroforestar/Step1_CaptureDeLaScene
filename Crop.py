8b6027d799d38d10834811525b359339b9d5007d
Laetitia LEM <laetitia.lemiere@gmail.com>
2022-01-11 15:19:32 +0100

    commit in wrong directory

--- Crop.py
+++
@@ -1,9 +0,0 @@
-from Plant import Plant
-
-class Crop(Plant):
-    def __init__(self, specie, Xcoord, Ycoord):
-        super().__init__(specie, Xcoord, Ycoord)
-        self.color = (0,125,125)
-
-    def __repr__(self):
-        return repr((self.X, self.Y))
